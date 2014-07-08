86b4c67cbb8a2e343a64744791f1582a3c2cf112
Erik J. Groeneveld <erik@seecr.nl>
2014-07-08 19:00:07 +0200

    extend_cct now ranges from 1,000-10,000 Kelvin

--- huecycle/extended_cct.py
+++ huecycle/extended_cct.py
@@ -5,16 +5,21 @@ def interpolate(lo, hi, x, lo2, hi2):
 
 @autostart
 def extend_cct(lamp):
-    HUE_AT_2000K = 12521 # set lamp to ct=1000000/2000, wait and get and sat hue from lamp
-    SAT_AT_2000K = 225
+    HUE_AT_1000K = 0     # follow the Planckian locus as close as possible towards the lower-right
+    SAT_AT_1000K = 255
     HUE_AT_1500K = 10500 # roughly where the Plackian locus exits Hue's gamut (y=0.4)
     SAT_AT_1500K = 255   # keep lowering hue until you get y=0.4 (and x=?), then get hue and sat
-    HUE_AT_1000K = 0
+    HUE_AT_2000K = 12521 # set lamp to ct=1000000/2000, wait and get and sat hue from lamp
+    SAT_AT_2000K = 225
+    HUE_AT_6500K = 34534 # set lamp to ct=1000000/6500, wait and get hue and sat from lamp
+    SAT_AT_6500K = 240
+    HUE_AT_10000K = 46920 # lower left corner of gamut, see http://developers.meethue.com/coreconcepts.html
+    SAT_AT_10000K = 255
 
     while True:
         args = yield
         ct = args["ct"]
-        if ct < 1000:
+        if not 1000 <= ct <= 10000:
             raise ValueError("CT value not supported: %d" % ct)
         if ct < 1500:
             hue = interpolate(1000, 1500, ct, HUE_AT_1000K, HUE_AT_1500K)
@@ -24,10 +29,10 @@ def extend_cct(lamp):
             sat = interpolate(HUE_AT_2000K, HUE_AT_1500K, hue, SAT_AT_2000K, SAT_AT_1500K)
             lamp.send(dict(hue=hue, sat=sat))
         elif ct < 6501:
-            lamp.send(dict(ct=ct))
+            lamp.send(dict(ct=1000000/ct))
         else:
-            hue = interpolate(6500, 10000, ct, 34534, 46920)
-            sat = interpolate(6500, 10000, ct, 240, 255)
+            hue = interpolate(6500, 10000, ct, HUE_AT_6500K, HUE_AT_10000K)
+            sat = interpolate(6500, 10000, ct, SAT_AT_6500K, SAT_AT_10000K)
             lamp.send(dict(hue=hue, sat=sat))
 
 class MockLamp(object):
@@ -36,6 +41,17 @@ class MockLamp(object):
 
 l = MockLamp()
 
+@gtest
+def testCheckUpperBoundary():
+    ex_cct = extend_cct(None)
+    yield
+    try:
+        ex_cct.send(dict(ct=10001))
+        raise Exception("must raise")
+    except Exception, e:
+        assert "CT value not supported: 10001" == str(e), str(e)
+    yield
+    
 @gtest
 def testSuperHueRange_6500_10000K():
     ex_cct = extend_cct(l)
@@ -52,11 +68,11 @@ def testNormalHueRange_2000_6500K():
     ex_cct = extend_cct(l)
     yield
     ex_cct.send(dict(ct=2000))
-    assert l.args == dict(ct=2000), l.args
+    assert l.args == dict(ct=500), l.args
     ex_cct.send(dict(ct=6500))
-    assert l.args == dict(ct=6500), l.args
+    assert l.args == dict(ct=153), l.args
     ex_cct.send(dict(ct=3750))
-    assert l.args == dict(ct=3750), l.args
+    assert l.args == dict(ct=266), l.args
     yield
     del ex_cct
 
@@ -97,8 +113,10 @@ def testCheckLowerBoundary():
         assert "CT value not supported: 999" == str(e), str(e)
     yield
 
-testSubHueRange_1000_1500K()
-testCheckLowerBoundary()
-testSubHueRange_1500_2000K()
-testNormalHueRange_2000_6500K()
-testSuperHueRange_6500_10000K()
+if __name__ == "__main__":
+    testSubHueRange_1000_1500K()
+    testCheckLowerBoundary()
+    testSubHueRange_1500_2000K()
+    testNormalHueRange_2000_6500K()
+    testSuperHueRange_6500_10000K()
+    testCheckUpperBoundary()

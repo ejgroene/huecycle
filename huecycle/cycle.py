cfcde606cf07da866f741b26196f5237ca3b578a
Erik Groeneveld <erik@seecr.nl>
2014-08-30 13:25:20 +0200

    EG: refactored and add lots of unittests

--- huecycle/cycle.py
+++ huecycle/cycle.py
@@ -1,55 +1,13 @@
 #!/usr/bin/env python
 from sys import stdout
 from time import sleep
-from datetime import datetime, time, timedelta
-from misc import autostart, lamp, attenuator, hours
-from config import LOCAL_HUE_API
-from extended_cct import extend_cct, MIREK
-from sunphase import rise_and_set
-from phase import phase, linear, sinus, charge, constant
-from itertools import izip
-
-"""
-We use MIREK instead of Kelvin as unit for color temperature because:
- - this is the unit used by Hue, and Hue just has so many levels
- - it ranges from 100-1000 in 900 steps instead of 9000 steps in Kelvin.
- - it is linear instead of exponential, so color changes more linear
-"""
-
-# http://www.apogeephoto.com/july2004/jaltengarten7_2004.shtml
-CCT_SUN_RISE = CTT_SUN_SET = MIREK/2000
-CCT_SUN_RISE_PLUS_1 = MIREK/3500
-CCT_EARLY_MORNING_LATE_AFTERNOON = MIREK/4300
-CCT_AVG_SUMMER_SUNLIGHT = MIREK/5400
-CCT_OVERCAST_SKY = MIREK/6000
-# my own
-CCT_RED_SUN = MIREK/1000
-CCT_DEEP_NIGHT = MIREK/10000
-
-def get_ct_phase(t_wake, t_sleep, t_rise, t_set, t):
-    t_dawn_end = min(t_wake, t_rise)
-    t_dusk_begin = max(t_sleep, t_set)
-    t_dawn_begin = t_dawn_end.replace(hour=t_dawn_end.hour - 1)
-    t_dusk_end = t_dusk_begin.replace(hour=t_dusk_begin.hour + 1)
-    if t_rise <= t < t_set:
-        print " * Just follow the sun's color."
-        return phase(t_rise, t_set, sinus(charge(2.)), CCT_SUN_RISE, CCT_AVG_SUMMER_SUNLIGHT)
-    if t_wake <= t < t_rise or t_set <= t < t_sleep:
-        print " * Awake but no sun."
-        return constant(CTT_SUN_SET)
-    if t_dawn_begin <= t < t_dawn_end:
-        print " * Morning twilight."
-        return phase(t_dawn_begin, t_dawn_end, linear(), CCT_RED_SUN, CCT_SUN_RISE)
-    if t_dusk_begin <= t < t_dusk_end:
-        print " * Evening twilight."
-        return phase(t_dusk_begin, t_dusk_end, linear(), CCT_SUN_SET, CCT_RED_SUN)
-    if t_dusk_end <= t < time.max or time.min <= t < t_dawn_begin:
-        print " * Night."
-        return phase(t_dusk_end, t_dawn_begin, sinus(), CCT_RED_SUN, CCT_DEEP_NIGHT)
-    raise Exception("Cannot match cycle: %s" % t)
+from datetime import datetime, time
 
 
 def loop(light):
+    from itertools import izip
+    from phase import phase, sinus, charge
+    from sunphase import get_ct_phase, rise_and_set, MIREK
     last_bri = last_ct = 0
     sun = rise_and_set(52.053055, 5.638889, -6.0)
     t_wake = time(7,15)
@@ -63,17 +21,19 @@ def loop(light):
                 last_bri = brightness
                 last_ct = color_temp
                 print "%s; %dK; %.1f%%" % (datetime.now().strftime("%a %H:%M:%S"), MIREK/color_temp, brightness/2.55)
-                light.send(dict(ct=color_temp, bri=brightness, on=True))
+            light.send(dict(ct=color_temp, bri=brightness, on=True))
             stdout.flush()
             sleep(1.0)
         sleep(1.0)
 
 
 if __name__ == "__main__":
-    group1 = lamp(LOCAL_HUE_API + "groups/0/action")
-    light1 = extend_cct(attenuator(group1))
-    #lamp1 = lamp(LOCAL_HUE_API + "lights/1/state")
-    #light1 = extend_cct(lamp1)
+    from misc import lamp, attenuator
+    from config import LOCAL_HUE_API
+    from extended_cct import extend_cct
+    lamp = lamp(LOCAL_HUE_API + "groups/0/action")
+    #lamp = lamp(LOCAL_HUE_API + "lights/1/state")
+    lights = extend_cct(attenuator(lamp))
     #weather1 = weather("http://api.wunderground.com/api/%s/conditions/q/pws:IUTRECHT57.json" % WUNDERGROUND_API_KEY)
-    loop(light1)
+    loop(lights)
 
--- huecycle/sunphase.py
+++ huecycle/sunphase.py
@@ -1,6 +1,7 @@
-from misc import autotest, autostart
+from misc import autotest, autostart, hours
 from datetime import datetime, date, time
 from ephem import Observer, Sun, Moon, localtime
+from phase import phase, sinus, charge, constant, linear
 
 @autostart
 def rise_and_set(lat, lon, horizon=0):
@@ -34,3 +35,144 @@ def testRiseAndSet():
     t_rise, t_set = sun.send(datetime(2014,6,21,12,00)) # make sure it finds latest, not next sun rise
     assert t_rise == time(5,16,55,000005), t_rise
     assert t_set == time(22,01,47,000005), t_set
+
+
+"""
+We use MIREK instead of Kelvin as unit for color temperature because:
+ - this is the unit used by Hue, and Hue just has so many levels
+ - it ranges from 100-1000 in 900 steps instead of 9000 steps in Kelvin.
+ - it is linear instead of exponential, so color changes more linear
+"""
+
+# http://www.apogeephoto.com/july2004/jaltengarten7_2004.shtml
+MIREK = 1000000
+CCT_SUN_RISE = CCT_SUN_SET = MIREK/2000
+CCT_SUN_RISE_PLUS_1 = MIREK/3500
+CCT_EARLY_MORNING_LATE_AFTERNOON = MIREK/4300
+CCT_AVG_SUMMER_SUNLIGHT = MIREK/5400
+CCT_OVERCAST_SKY = MIREK/6000
+# my own
+CCT_RED_SUN = MIREK/1000
+CCT_DEEP_NIGHT = MIREK/10000
+
+def get_ct_phase(t_wake, t_sleep, t_rise, t_set, t):
+    t_dawn_end = min(t_wake, t_rise)
+    t_dusk_begin = max(t_sleep, t_set)
+    t_dawn_begin = t_dawn_end.replace(hour=t_dawn_end.hour - 1)
+    t_dusk_end = t_dusk_begin.replace(hour=t_dusk_begin.hour + 1)
+    if t_rise <= t < t_set:
+        return phase(t_rise, t_set, sinus(charge(2.)), CCT_SUN_RISE, CCT_AVG_SUMMER_SUNLIGHT)
+    if t_wake <= t < t_rise or t_set <= t < t_sleep:
+        return constant(CCT_SUN_SET)
+    if t_dawn_begin <= t < t_dawn_end:
+        return phase(t_dawn_begin, t_dawn_end, linear(), CCT_RED_SUN, CCT_SUN_RISE)
+    if t_dusk_begin <= t < t_dusk_end:
+        return phase(t_dusk_begin, t_dusk_end, linear(), CCT_SUN_SET, CCT_RED_SUN)
+    if t_dusk_end <= t < time.max or time.min <= t < t_dawn_begin:
+        return phase(t_dusk_end, t_dawn_begin, sinus(), CCT_RED_SUN, CCT_DEEP_NIGHT)
+    raise Exception("Cannot match cycle: %s" % t)
+
+
+def summer_phase(t):
+    return get_ct_phase(time(7), time(21), time(6), time(22), t)
+@autotest
+def summer_day():
+    ct = summer_phase(time(06,00)).send(time(6,00))
+    assert ct == CCT_SUN_RISE, ct
+    ct = summer_phase(time(06,01)).send(time(6,01))
+    assert ct == 498, ct
+    ct = summer_phase(time(21,59)).send(time(21,59))
+    assert ct == 498, ct
+
+@autotest
+def summer_morning_twilight():
+    ct = summer_phase(time(05,00)).send(time(5,00))
+    assert ct == 1000, ct
+    ct = summer_phase(time(05,01)).send(time(5,01))
+    assert ct == 992, ct
+    ct = summer_phase(time(05,59)).send(time(5,59))
+    assert ct == 508, ct
+
+@autotest
+def summer_evening_twilight():
+    ct = summer_phase(time(22,00)).send(time(22,00))
+    assert ct == 500, ct
+    ct = summer_phase(time(22,01)).send(time(22,01))
+    assert ct == 508, ct
+    ct = summer_phase(time(22,59)).send(time(22,59))
+    assert ct == 992, ct
+  
+@autotest
+def summer_night():
+    ct = summer_phase(time(23,00)).send(time(23,00))
+    assert ct == 1000, ct
+    ct = summer_phase(time(23,59)).send(time(23,59))
+    assert ct == 557, ct
+    ct = summer_phase(time(00,00)).send(time(00,00))
+    assert ct == 550, ct
+    ct = summer_phase(time(00,01)).send(time(00,01))
+    assert ct == 543, ct
+    ct = summer_phase(time(02,00)).send(time(02,00))
+    assert ct == 100, ct
+    ct = summer_phase(time(04,59)).send(time(04,59))
+    assert ct == 992, ct
+
+def winter_phase(t):
+    return get_ct_phase(time(7), time(21), time(8), time(19), t)
+
+@autotest
+def winter_day():
+    ct = winter_phase(time( 8,00)).send(time( 8,00))
+    assert ct == 500, ct
+    ct = winter_phase(time( 8,01)).send(time( 8,01))
+    assert ct == 497, ct
+    ct = winter_phase(time(18,59)).send(time(18,59))
+    assert ct == 497, ct
+   
+@autotest
+def winter_morning_twilight_and_snooze():
+    ct = winter_phase(time(06,00)).send(time(6,00))
+    assert ct == 1000, ct
+    ct = winter_phase(time(06,01)).send(time(6,01))
+    assert ct ==  992, ct
+    ct = winter_phase(time(06,59)).send(time(6,59))
+    assert ct ==  508, ct
+    ct = winter_phase(time(07,00)).send(time(7,00))
+    assert ct ==  500, ct
+    ct = winter_phase(time(07,30)).send(time(7,30))
+    assert ct ==  500, ct
+    ct = winter_phase(time(07,59)).send(time(7,59))
+    assert ct ==  500, ct
+
+@autotest
+def winter_evening_snooze_and_twilight():
+    ct = winter_phase(time(19,00)).send(time(19,00))
+    assert ct ==  500, ct
+    ct = winter_phase(time(20,00)).send(time(20,00))
+    assert ct ==  500, ct
+    ct = winter_phase(time(20,59)).send(time(20,59))
+    assert ct ==  500, ct
+    ct = winter_phase(time(21,00)).send(time(21,00))
+    assert ct ==  500, ct
+    ct = winter_phase(time(21,01)).send(time(21,01))
+    assert ct ==  508, ct
+    ct = winter_phase(time(21,59)).send(time(21,59))
+    assert ct ==  992, ct
+
+@autotest
+def winter_night():
+    ct = winter_phase(time(22,00)).send(time(22,00))
+    assert ct == 1000, ct
+    ct = winter_phase(time(22,01)).send(time(22,01))
+    assert ct ==  994, ct
+    ct = winter_phase(time(23,59)).send(time(23,59))
+    assert ct ==  368, ct
+    ct = winter_phase(time(00,00)).send(time(00,00))
+    assert ct ==  364, ct
+    ct = winter_phase(time(00,01)).send(time(00,01))
+    assert ct ==  359, ct
+    ct = winter_phase(time(02,00)).send(time(02,00))
+    assert ct ==  100, ct
+    ct = winter_phase(time(05,59)).send(time(05,59))
+    assert ct ==  994, ct
+

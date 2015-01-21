9692fc745e45323b1dcb9b023447ecbe606d8fa7
Erik Groeneveld <erik@seecr.nl>
2015-01-21 13:24:39 +0100

    EG: using delegation/prototyping for sensors

--- huecycle/prototype.py
+++ huecycle/prototype.py
@@ -16,31 +16,47 @@ class prototype(object_):
     def __init__(self, proto, ctor, **kwargs):
         self.__prototypes__ = (self,) + (proto.__prototypes__ if proto else ())
         if ctor:
-            ctor(self)
             for code in (c for c in ctor.func_code.co_consts if iscode(c)):
-                setattr(self, code.co_name, FunctionType(code, dict(), code.co_name))
+                setattr(self, code.co_name, FunctionType(code, ctor.func_globals, code.co_name))
+            ctor(self)
         self.__dict__.update(kwargs)
             
 
+    def __call__(self, function=None, **kwargs):
+        return prototype(self, function, **kwargs)
+
+    def __contains__(self, name):
+        return name in self.__dict__
+
+    def __repr__(self):
+        return repr(self.__dict__)
+
+    def update(self, kws):
+        return self.__dict__.update(kws)
+
 def object(*args, **kwargs):
-    if args and isinstance(args[0], FunctionType):
-        return lambda proto=None: prototype(proto, args[0], **kwargs)
+    if args:
+        if isinstance(args[0], FunctionType):
+            return prototype(None, args[0], **kwargs)
+        else:
+            return prototype(args[0], None, **kwargs)
     else:
-        return prototype(args[0] if args else None, None, **kwargs)
-
-@object
-def myobject1(self, c=42):
-    self.prop = 1+2
-    self.prak = "aa"
-    def f(self):
-        return self
-
-@object
-def myobject2(self, c=43):
-    def g(self):
-        return self
+        return prototype(None, None, **kwargs)
 
 from misc import autotest
+
+@autotest
+def CreateObjectFromPrototype():
+    creature = object(alive=True)
+    @creature
+    def person(self):
+        def age(self): return 2015 - self.birth
+    me = object(person, birth=1990)
+    assert me.age() == 25, me
+    assert me.alive == True
+    me.alive = False
+    assert me.alive == False
+    assert creature.alive == True
 
 @autotest
 def FindPrototypes():
@@ -62,17 +78,49 @@ def FindPrototypes():
     
 @autotest
 def CreatePrototype():
-    p1 = myobject1()
+    @object
+    def p1(self):
+        self.prop = 1 + 2
+        self.prak = "aa"
+        self.this = self.f()
+        def f(self): return self
     assert p1
     assert p1.f()
     assert p1 == p1.f()
     assert p1.prop == 3, p1.prop
     assert p1.prak == "aa"
-    p2 = myobject2(p1)
+    assert p1.this == p1
+    @p1
+    def p2(self):
+        def g(self): return self
     assert p2
     assert p2.__prototypes__ == (p2, p1), p2.__prototypes__
     assert p2.prop == 3
     assert p2.f() == p2
     assert p2.g() == p2
-    assert p1.c == 42, p1.c
-    assert p2.c == 43
+    assert p2.this == p1
+
+@autotest
+def CallPrototypeCreateNewObject():
+    p = object()
+    o = p(a=42)
+    assert o
+    assert o != p
+    assert o.__prototypes__ == (o, p)
+    assert o.a == 42
+
+@autotest
+def UseGlobals():
+    @object
+    def one(self):
+        def f(self):
+            return autotest
+    assert one.f() == autotest
+
+@autotest
+def AccessProperties():
+    o = object(a=1, b=2)
+    assert "a" in o
+    assert "b" in o
+    assert "c" not in o
+    assert str(o) == "{'a': 1, 'b': 2, '__prototypes__': ({...},)}", str(o)
--- huecycle/sensors.py
+++ huecycle/sensors.py
@@ -1,5 +1,6 @@
-from requests import post
+from rest import get, post, put
 from json import dumps
+from prototype import object
 
 def delete_all_sensors():
     sensors = get(LOCAL_HUE_API + "/sensors")
@@ -10,4 +11,52 @@ def create_sensor(API, name, type):
     r = post(API + "/sensors", dumps(dict(name=name, type=type, modelid=type,
                                     manufacturername="ErikGroeneveld", swversion="0.1", uniqueid=name)))
     return r.json[0]["success"]["id"]
+
+@object
+def sensor(self):
+    def address(self): return "/sensors/%s" % self.id
+    def url(self): return self.baseurl + self.address()
+    def info(self): return get(self.url())
+    def state(self): return self.info()["state"]
+    def send(self, part, **data):
+        put(self.url() + part, **data)
+    def config(self):
+        @self
+        def config_state(self):
+            self.update(self.info()["config"])
+            def set(self, val):
+                self.send("/config", on=val)
+        return config_state
+
+@sensor
+def tap(self):
+    pass
+
+from misc import autotest
+from config import LOCAL_HUE_API
+
+@autotest
+def ReadTap():
+    tap1 = tap(id=2)
+    assert tap1.id == 2
+    assert tap1.address() == "/sensors/2"
+    tap1.baseurl = LOCAL_HUE_API
+    state = tap1.state()
+    assert "lastupdated" in state
+    assert "buttonevent" in state
+
+@autotest
+def ReadAndWriteConfig():
+    tap1 = tap(id=2)
+    tap1.baseurl = LOCAL_HUE_API
+    config = tap1.config()
+    assert "on" in config
+    assert config.on in (True, False)
+    config.set(False)
+    config = tap1.config()
+    assert config.on == False
+    config.set(True)
+    config = tap1.config()
+    assert config.on == True
+
 
